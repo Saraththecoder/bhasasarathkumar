a21805ec2bb2be3027df68cda518b32767187bd8
Saraththecoder <sarathkumar86518@gmail.com>
2026-05-26 11:43:18 +0530

    Fix splitText crash by removing br tag from h1 name

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,7 +9,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              BHASA<br />SARATH KUMAR
+              BHASA SARATH KUMAR
             </h1>
           </div>
           <div className="landing-info">
